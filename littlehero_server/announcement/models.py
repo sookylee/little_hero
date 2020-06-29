3f22db6b6b80a73d9cd79eedb7961f10d9464d8e
SooKyeongLee <bbluv_@naver.com>
2020-06-29 10:05:06 +0900

    start django proj, and made announcement model

---
+++ littlehero_server/announcement/models.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    address_city = models.CharField(max_length=100)
+    address_gu = models.CharField(max_length=100)
+    address_remainder = models.CharField(max_length=200, default='')
+    recruit_status = models.BooleanField(default=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    end_date = models.DateTimeField(
+            blank=True, null=True)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
