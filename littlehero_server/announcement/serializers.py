312d6b04fc84970189e2443ae4affd11ba01e89b
sookylee <bbluv_@naver.com>
2020-07-01 14:47:01 +0900

    changed easier

--- littlehero_server/announcement/serializers.py
+++ littlehero_server/announcement/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from .models import Post
-
+from django_filters import rest_framework as filters
 
 class PostSerializer(serializers.ModelSerializer) :
     class Meta :
