f3028c57a94d1b2e96f5d0ad962f084d5e615673
sookylee <bbluv_@naver.com>
2020-07-01 12:15:18 +0900

    made view.

--- littlehero_server/announcement/views.py
+++ littlehero_server/announcement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+class PostViewAll(viewsets.ModelViewSet) :
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer) :
+        serializer.save(user=self.request.user)
