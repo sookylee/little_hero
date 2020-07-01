94dce263b53f4530c780dd4ce14fd8ac2230a73b
sookylee <bbluv_@naver.com>
2020-07-01 17:15:01 +0900

    fixed insert function

--- littlehero_server/_db_utils.py
+++ littlehero_server/_db_utils.py
@@ -18,23 +18,22 @@ def push_data(data) :
     ## push
     else : 
         Post(
-                regist_no = data['regist_no'],
-                title = data['title'],
-                recruit_status = data['recruit_status'],
-                adult_status = data['adult_status'],
-                domain = data['domain'],
-                text = data['text'],
-                do_date = data['do_date'],
-                do_time = data['do_time'],
-                do_week = data['do_week'],
-                recruit_date = data['recruit_date'],
-                recruit_member = data['recruit_member'],
-                recruit_company = data['recruit_company'],
-                telephone = data['telephone'],
-                address_city = data['address_city'],
-                address_gu = data['address_gu'],
-                address_remainder = data['address_remainder'],
-                url = data['url']
+                site_domain=data['site_domain'],
+                regist_no=data['regist_no'],
+                title=data['title'],
+                recruit_status=data['recruit_status'],
+                adult_status=data['adult_status'],
+                domain=data['domain'],
+                text=data['text'],
+                do_date=data['do_date'],
+                do_date_extra=data['do_date_extra'],
+                recruit_member=data['recruit_member'],
+                recruit_company=data['recruit_company'],
+                telephone=data['telephone'],
+                address_city=data['address_city'],
+                address_gu=data['address_gu'],
+                address_remainder=data['address_remainder'],
+                url=data['url']
             ).save()
         print(data +' has pushed.')
 
