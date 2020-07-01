5243b70e2cf372e20e48fbe23b34b80e73eeaf40
sookylee <bbluv_@naver.com>
2020-07-01 14:49:50 +0900

    changed easier

--- littlehero_server/announcement/serializers.py
+++ littlehero_server/announcement/serializers.py
@@ -5,6 +5,3 @@ class PostSerializer(serializers.ModelSerializer) :
     class Meta :
         model = Post
         fields = '__all__'
-        read_only_fields = '__all__'
-        #light_only_fields=(
-        # )
