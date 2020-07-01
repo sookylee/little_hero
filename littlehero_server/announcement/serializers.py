e3e8af711c372b8eeca367e2f42cbce25439a6ad
sookylee <bbluv_@naver.com>
2020-07-01 14:30:56 +0900

    fixed serializer

--- littlehero_server/announcement/serializers.py
+++ littlehero_server/announcement/serializers.py
@@ -5,23 +5,6 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer) :
     class Meta :
         model = Post
-        fields = (
-            'url',
-            'title',
-            'address_city',
-            'address_gu',
-            'address_remainder',
-            'recruit_status',
-            'adult_status',
-            'domain',
-            'telephone',
-            'do_date',
-            'do_time',
-            'do_week',
-            'recruit_date',
-            'recruit_company',
-            'recruit_member',
-            'text',
-        )
+        fields = '__all__'
         #light_only_fields=(
         # )
