987db6d8e3f90f209dbb7ea2fbb03e992f655093
sookylee <bbluv_@naver.com>
2020-07-10 08:57:24 +0900

    update url and view

--- littlehero_server/littlehero_server/urls.py
+++ littlehero_server/littlehero_server/urls.py
@@ -25,7 +25,7 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', get_swagger_view(title='Rest API Document')),
+    url(r'^api/doc', get_swagger_view(title='Rest API Document')),
     url(r'^api/posts', PostView.as_view()),
     url(r'^api/posts/detail', PostViewDetail.as_view()),
 ]
